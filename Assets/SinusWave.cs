98181e6ff0cbdc6f554a1013bf6de03b6ca5f2d8
DESKTOP-KDQR32S\Targendorf <noco4ki@mail.ru>
2021-03-06 20:42:55 +0200

    Pepega

---
+++ Assets/SinusWave.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class SinusWave : MonoBehaviour {
+
+    public int lengthOfLineRenderer = 20;
+    void Start() {
+        LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.SetWidth(0.1F, 0.2F);
+        lineRenderer.SetVertexCount(lengthOfLineRenderer);
+    }
+    void Update() {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        int i = 0;
+        while (i < lengthOfLineRenderer) {
+            Vector3 pos = new Vector3(i * 0.5F, Mathf.Sin(i + Time.time), 0);
+            lineRenderer.SetPosition(i, pos);
+            i++;
+        }
+    }
+}
